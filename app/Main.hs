3f8e395e3ef4ae50d3da2df1b87829a30505add5
Erik Rasmussen <ecr.referee@hotmail.com>
2021-06-12 16:02:49 -0500

    Hacking through crap

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,55 @@
 module Main where
 
+import Control.Monad.Loops
 import Lib
+
+height :: Integer
+height = 20
+
+screenHeight :: Integer
+screenHeight = height + 2
+
+width :: Integer
+width = height
+
+screenWidth :: Integer
+screenWidth = width + 3
+
+data Tile = Empty | Snake | Apple
+  deriving (Eq, Show)
+
+tileToChar :: Tile -> Char
+tileToChar Empty = 'e'
+tileToChar Snake = 's'
+tileToChar Apple = 'a'
+
+board :: [Tile]
+board = [Empty | _ <- [0..(10 * 10)]]
+
+onBoard :: Integer -> Bool
+onBoard x
+  | x < screenWidth = False
+  | x > (screenWidth * (screenHeight - 1)) = False
+  | mod x screenWidth == 1 = False
+  | mod x screenWidth < (width + 2) = True
+  | otherwise = False
+
+toTile :: Integer -> Char
+toTile x
+  | mod x screenWidth == 0 = '\n'
+  | onBoard x = ' '
+  | otherwise = '*'
+
+draw :: IO ()
+draw = putStr [toTile x | x <- [0..(screenWidth * screenHeight)]]
+
+menu :: IO ()
+menu = do
+  putStrLn "s -> Start"
+  putStrLn "q -> Quit"
 
 main :: IO ()
 main = do
-  putStrLn "Hello World"
-  
+  menu
+  choice <- getLine
+  draw
